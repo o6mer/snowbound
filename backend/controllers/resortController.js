b1c0748df74910f63a2c186b37f645f20c206edd
MarkGrinberg1911 <mark.grinberg703@gmail.com>
2023-03-01 16:43:28 +0200

    marks commit

--- backend/controllers/resortController.js
+++ backend/controllers/resortController.js
@@ -1,4 +1,5 @@
 const db = require("../models/dbModel");
+const format = require('pg-format');
 
 const getResortByBame = async (req, res) => {
   const { name } = req.params;
@@ -18,6 +19,41 @@ const getResortByBame = async (req, res) => {
   }
 };
 
-const getMultipleResortByBame = async (req, res) => {};
+const getMultipleResortByBame = async (req, res) => {
+  const { names } = req.body;
+
+  
+  if (!names) return res.status(400).json({ message: "names not provided" });
+
+  try {
+    const { rows } = await db.query(format(`SELECT * FROM resort WHERE name in %L`, [names]));
+    console.log(rows);
+    res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ message: err.message });
+  }
+};
+
+const getResortByCountry = async (req, res) => {
+  const { resorts } = req.params;
+  console.log(req.params)
+
+  if (!resorts) return res.status(400).json({ message: "name not provided" });
+
+  try {
+    const { rows } = await db.query("SELECT * FROM resort WHERE countryId = $1", [
+      resorts,
+    ]);
+
+    console.log(rows);
+    res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ message: err.message });
+  }
+};
 
 exports.getResortByBame = getResortByBame;
+exports.getMultipleResortByBame= getMultipleResortByBame;
+exports.getResortByCountry=getResortByCountry;
--- backend/routes/resortRouter.js
+++ backend/routes/resortRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { getResortByBame } = require("../controllers/resortController");
 const resortRouter = express.Router();
+const { getResortByBame, getMultipleResortByBame, getResortByCountry } = require("../controllers/resortController");
 
-resortRouter.get("/:name", getResortByBame);
+resortRouter.get("/find/:name", getResortByBame);
+resortRouter.get("/find/country/:country", getResortByCountry);
+resortRouter.get("/compare", getMultipleResortByBame);
 
 module.exports = resortRouter;
