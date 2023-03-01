d4775af0f60082f5244f35eec8df3b4586f191cb
o6mer <o6merliraz@gmail.com>
2023-03-01 10:44:19 +0200

    routes setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,47 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./general/error-page";
 import UserContextProvider from "./contexts/UserContextProvider";
+import SearchPage from "./search/SearchPage";
+import ComparePage from "./compare/ComparePage";
+import ResortPage from "./resort/ResortPage";
+import AdminPage from "./admin/AdminPage";
+import LoginPage from "./userAuth/LoginPage";
+import SignupPage from "./userAuth/SignupPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/search/:query",
+    element: <SearchPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/compare/:query",
+    element: <ComparePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/resort/:name",
+    element: <ResortPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/admin",
+    element: <AdminPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
     errorElement: <ErrorPage />,
   },
 ]);
---
+++ frontend/src/resort/ResortPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResortPage = () => {
+  return <div>ResortPage</div>;
+};
+
+export default ResortPage;
