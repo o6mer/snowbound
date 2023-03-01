a1308643a4362332408cbc867f5997602ffbeabb
roeiu100 <roeiu100@gmail.com>
2023-03-01 15:06:47 +0200

    navbar

--- frontend/src/search/SearchPage.jsx
+++ frontend/src/search/SearchPage.jsx
@@ -1,7 +1,103 @@
 import React from "react";
+import Navbar from "../general/Navbar";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <div>
+      <Navbar />
+      <header class="w-full h-96 bg-[url('./assets/HeroSearch.png')] bg-cover bg-center flex justify-center items-center">
+        <div class="flex flex-col justify-center items-center">
+          <h1 class=" text-center text-3xl text-white font-bold drop-shadow-lg">
+            Here you can search for your the
+            <br /> best vecation that suits for you
+            <br />
+            <span class="text-blue-500">Feel free</span>
+          </h1>
+          <p class="mt-5 text-center text-lg text-blue opacity-70">
+            Plese choose the country you want to visit
+          </p>
+          <form class="flex items-center">
+            <label for="simple-search" class="sr-only">
+              Search
+            </label>
+            <div class="relative w-full">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="simple-search"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <svg
+                class="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+              <span class="sr-only">Search</span>
+            </button>
+          </form>
+        </div>
+      </header>
+      <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <ul class="flex flex-wrap -mb-px">
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+            >
+              Continent
+            </a>
+          </li>
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+              aria-current="page"
+            >
+              Country
+            </a>
+          </li>
+          <li class="mr-2">
+            <a
+              href="#"
+              class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+            >
+              Resort
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
