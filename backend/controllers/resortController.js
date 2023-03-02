dcafb9947e2caa21d8c9ead713c2302d9a930f44
MarkGrinberg1911 <mark.grinberg703@gmail.com>
2023-03-02 19:15:30 +0200

    added updated quary

--- backend/controllers/resortController.js
+++ backend/controllers/resortController.js
@@ -77,7 +77,6 @@ const createResort = (req, res) => {
   const resort = req.body;
   if (!resort) return res.status(400).json({ message: "names not provided" });
   try {
-    // console.log(format("INSERT INTO resort (%I) VALUES (%L)", Object.keys(resort), Object.values(resort)))
     const { rows } = db.query(format("INSERT INTO resort (%I) VALUES (%L)", Object.keys(resort), Object.values(resort)));
     console.log(rows);
     res.status(200).json(rows);
@@ -87,11 +86,46 @@ const createResort = (req, res) => {
     res.status(404).json({ message: err.message });
   }
 }
+// const updateResort = (req, res) => {
+//   console.log("this is:", req.body.name)
+//   const resort = req.body.values;
+//   qName = req.body.name.name;
+//   if (!resort) return res.status(400).json({ message: "names not provided" });
+//   try {
+//     const { rows } = db.query(format(`UPDATE resort SET (%I) VALUES (%L) WHERE name=${qName}`, Object.keys(resort), Object.values(resort)));
+//     console.log(rows);
+//     res.status(200).json(rows);
+
+//   } catch (err) {
+//     console.log(err);
+//     res.status(404).json({ message: err.message });
+//   }
+// }
+const updateResort = async (req, res) => {
+  console.log("this is:", req.body.name)
+  const resort = req.body.values;
+  const qName = req.body.name.name;
+  if (!resort) return res.status(400).json({ message: "names not provided" });
+  try {
+    const keys = Object.keys(resort);
+    const values = Object.values(resort);
+    const columns = keys.map((key) => format("%I = %L", key, resort[key])).join(", ");
+    const query = format(`UPDATE resort SET ${columns} WHERE name = %L RETURNING *`, qName);
+    const { rows } = await db.query(query);
+    console.log(rows);
+    res.status(200).json(rows);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ message: err.message });
+  }
+}
+
 
 module.exports = {
   getResortByCountry,
   getMultipleResortByBame,
   getResortByBame,
   deleteResortByBame,
-  createResort
+  createResort,
+  updateResort
 };
--- backend/routes/resortRouter.js
+++ backend/routes/resortRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const resortRouter = express.Router();
-const { getResortByBame, getMultipleResortByBame, getResortByCountry, deleteResortByBame, createResort } = require("../controllers/resortController");
+const { getResortByBame, updateResort, getMultipleResortByBame, getResortByCountry, deleteResortByBame, createResort } = require("../controllers/resortController");
 
 resortRouter.get("/find/:name", getResortByBame);
 resortRouter.get("/find/country/:country", getResortByCountry);
 resortRouter.get("/compare", getMultipleResortByBame);
 resortRouter.get("/delete/:name", deleteResortByBame);
 resortRouter.post("/create", createResort);
+resortRouter.post("/update", updateResort);
+
 
 module.exports = resortRouter;
