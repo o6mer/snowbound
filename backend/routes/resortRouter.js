d5c941471ebdd4e1623e06b67ddddcadd08aaa96
MarkGrinberg1911 <mark.grinberg703@gmail.com>
2023-03-02 16:30:19 +0200

    mark back 2

--- backend/routes/resortRouter.js
+++ backend/routes/resortRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const resortRouter = express.Router();
-const { getResortByBame, getMultipleResortByBame, getResortByCountry } = require("../controllers/resortController");
+const { getResortByBame, getMultipleResortByBame, getResortByCountry, deleteResortByBame, createResort } = require("../controllers/resortController");
 
 resortRouter.get("/find/:name", getResortByBame);
 resortRouter.get("/find/country/:country", getResortByCountry);
 resortRouter.get("/compare", getMultipleResortByBame);
+resortRouter.get("/delete/:name", deleteResortByBame);
+resortRouter.post("/create", createResort);
 
 module.exports = resortRouter;
