ca6a2a7b747a0889412d0eae25855f59d795cf89
roeiu100 <roeiu100@gmail.com>
2023-03-02 16:33:00 +0200

    HomePage

--- frontend/src/home/Components/HomeHero.jsx
+++ frontend/src/home/Components/HomeHero.jsx
@@ -1,69 +1,86 @@
-import React from 'react'
+import React from "react";
 
 const HomeHero = () => {
   return (
-    <header className="w-full h-200px bg-[url('./assets/HeroHome.jpg')] bg-cover bg-[0px_-200px] bg-no-repeat  flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className=" text-center text-3xl text-white font-bold drop-shadow-lg">
-          Welcome to SnowBound
-          <br />
-          <span className="text-blue-500">Feel free</span>
-        </h1>
-        <p className="mt-5 text-center text-lg text-orange-600	 font-normal		">
-          This Website is about finding you your best vecation.
-        </p>
-        <form className="flex items-center">
-          <label htmlFor="simple-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative w-full">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
+    <div className="relative bg-[url('./assets/HeroHome.jpg')] bg-cover bg-[0px_-100px] bg-no-repeat  flex justify-center items-center">
+      <div className="absolute inset-x-0 bottom-0">
+        <svg
+          viewBox="0 0 224 12"
+          fill="currentColor"
+          className="w-full -mb-1 text-white"
+          preserveAspectRatio="none"
+        >
+          <path d="M0,0 C48.8902582,6.27314026 86.2235915,9.40971039 112,9.40971039 C137.776408,9.40971039 175.109742,6.27314026 224,0 L224,12.0441132 L0,12.0441132 L0,0 Z" />
+        </svg>
+      </div>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
+          <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-blue-400 sm:text-4xl sm:leading-none">
+            Welcome to SnowBound
+            <br className="hidden md:block" />
+            We are here for you to give you
+            <br className="hidden md:block" />
+            any information you need{" "}
+            <span className="relative inline-block">
+              about ski vecation.{" "}
+              <div className="w-full h-3 -mt-3 bg-deep-purple-accent-400" />
+            </span>
+          </h2>
+          <p className="mb-6 text-base font-semibold tracking-wide text-black-300 md:text-lg">
+            Certainly! At our ski resort website, we strive to provide you with
+            comprehensive and informative comparisons between various ski
+            resorts in the area, helping you make an informed decision when
+            planning your next ski vacation.
+          </p>
+          <form className="flex flex-col items-center w-full mb-4 md:flex-row md:px-16">
             <input
-              type="text"
-              id="simple-search"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search"
+              placeholder="Search for a resort..."
               required
+              type="text"
+              className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 bg-transparent border-2 border-gray-400 rounded appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-200 focus:outline-none focus:shadow-outline"
             />
-          </div>
-          <button
-            type="submit"
-            className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto  bg-blue-500 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
+            >
+              Search
+            </button>
+          </form>
+          <input
+            placeholder="Comapre"
+            required
+            type="text"
+            className="flex-grow  h-12 px-4 mb-3 text-white transition duration-200 bg-transparent border-2 border-gray-400 rounded appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-200 focus:outline-none focus:shadow-outline"
+          />
+          <input
+            placeholder="Find"
+            required
+            type="text"
+            className="flex-grow h-12 px-4 mb-3 text-white transition duration-200 bg-transparent border-2 border-gray-400 rounded appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-200 focus:outline-none focus:shadow-outline"
+          />
+          <br />
+          <button className="text-blue-700  hover:text-blue-900">
+            Need help?
+          </button>
+          <a
+            href="#recomendedImg"
+            aria-label="Scroll down"
+            className="flex items-center justify-center my-10	 bg-blue-500 hover:bg-blue-700 w-10 h-10 mx-auto text-white duration-300 transform border border-gray-400 rounded-full hover:text-teal-accent-400 hover:border-teal-accent-400 hover:shadow hover:scale-110"
           >
             <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="currentColor"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
+              <path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z" />
             </svg>
-            <span className="sr-only">Search</span>
-          </button>
-        </form>
+          </a>
+        </div>
       </div>
-    </header>
+    </div>
   );
-}
+};
 
-export default HomeHero
+export default HomeHero;
--- frontend/src/home/HomePage.jsx
+++ frontend/src/home/HomePage.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Navbar from "../general/Navbar";
 import HomeHero from "./Components/HomeHero";
+import RecomendedResorts from "./Components/RecomendedResorts";
 
 const HomePage = () => {
-  return <div className="w-full h-full	">
-    <Navbar/>
-    <HomeHero/>
-  </div>;
+  return (
+    <div className="w-full h-full	">
+      <Navbar />
+      <HomeHero />
+      <div style={{ display: "flex" }}>
+      <RecomendedResorts place={"start"} />
+      <RecomendedResorts place={"center"} />
+      <RecomendedResorts place={"end"} />
+      </div>
+      <div style={{ display: "flex" }}>
+      <RecomendedResorts place={"start"} />
+      <RecomendedResorts place={"center"} />
+      <RecomendedResorts place={"end"} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
