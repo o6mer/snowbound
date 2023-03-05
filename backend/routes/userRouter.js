e3312903c75ca15a6bee2e6e44888cd61c24d5d1
MarkGrinberg1911 <mark.grinberg703@gmail.com>
2023-03-05 11:59:09 +0200

    added user functions

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const userRouter = express.Router();
+const { createUser, updateChecklist, login } = require("../controllers/userController");
+
+
+
+
+userRouter.post("/register", createUser);
+userRouter.post("/updateList", updateChecklist);
+userRouter.post("/login", login);
 
 module.exports = userRouter;
