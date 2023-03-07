d8405440910587d95745d4cf8d86671f59d9e56f
BPAdiv <adiv.balas@gmail.com>
2023-03-07 13:31:57 +0200

    fix

---
+++ frontend/src/admin/components/AdminHome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../../home/Components/";
+
+const AdminHome = () => {
+  return (
+    <>
+      <SearchBar />
+      <h1>Admin Home</h1>
+    </>
+  );
+};
+
+export default AdminHome;
