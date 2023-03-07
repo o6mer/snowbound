a7674f0af3b1c75304304a6b1bc61f1bc1a84fe0
BPAdiv <adiv.balas@gmail.com>
2023-03-07 15:07:32 +0200

    admin home SearchBar

--- frontend/src/admin/components/AdminHome.jsx
+++ frontend/src/admin/components/AdminHome.jsx
@@ -1,11 +1,42 @@
-import React from "react";
-import SearchBar from "../../home/Components/";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SearchBar from "../../home/Components/SearchBar";
+import AdminResorts from "./AdminResorts";
 
 const AdminHome = () => {
+  // useEffect(() => {
+  //   axios.post("http://localhost:5000/api/admin/getallresorts")
+  //   .then((res) => {})
+  //   .catch((err) => {})
+  // }, [])
+
+  const allResorts = ["jnjnjnjn", "Bangalore"];
+  const [adminResorts, setAdminResorts] = useState([]);
+  const [isToast, setIsToast] = useState(false);
+  useEffect(() => {
+    setAdminResorts(allResorts);
+  }, []);
+
+  const handleSearch = (e, resortName) => {
+    console.log(resortName);
+    const filterResort = allResorts.filter((res) => {
+      return res === resortName;
+    });
+    if (resortName) {
+      console.log(filterResort);
+      setAdminResorts(filterResort);
+      setIsToast(false);
+    } else {
+      setIsToast(true);
+      setAdminResorts(allResorts);
+    }
+  };
+
   return (
     <>
-      <SearchBar />
       <h1>Admin Home</h1>
+      <SearchBar handleSearch={handleSearch} isToast={isToast} />
+      <AdminResorts adminResorts={adminResorts} />
     </>
   );
 };
--- frontend/src/home/Components/SearchBar.jsx
+++ frontend/src/home/Components/SearchBar.jsx
@@ -2,29 +2,26 @@ import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import CloseIcon from '@mui/icons-material/Close';
-export default function SearchBar() {
+import CloseIcon from "@mui/icons-material/Close";
+import ClearIcon from "@mui/icons-material/Clear";
+export default function SearchBar({ handleSearch, isToast }) {
   //const [allResorts, setAllResorts] = useState([]);
   const [resortName, setResortName] = useState("");
   const [toast, setToast] = useState(false);
   const navigate = useNavigate();
-//   useEffect(() => {
-//   axios.get("http://localhost:5000/api/resort/getAllResorts")
-//   .then((res) => {
-//     console.log(res);
-//     setAllResorts(res.data)
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-//   }, [])
-  const allResorts=[
- "jnjnjnjn"
-    ,
-  "Bangalore"
-]
+  //   useEffect(() => {
+  //   axios.get("http://localhost:5000/api/resort/getAllResorts")
+  //   .then((res) => {
+  //     console.log(res);
+  //     setAllResorts(res.data)
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  //   }, [])
+  const allResorts = ["jnjnjnjn", "Bangalore"];
   const handleSelect = (event, value) => {
     if (value) {
       setResortName(value);
@@ -33,11 +30,7 @@ export default function SearchBar() {
   const showResort = (e) => {
     e.preventDefault();
     console.log(resortName);
-    if (
-      allResorts.find((resort) =>
-        resort === resortName
-      )
-    ) {
+    if (allResorts.find((resort) => resort === resortName)) {
       navigate(`/resort/${resortName}`);
     } else {
       setToast(true);
@@ -46,6 +39,11 @@ export default function SearchBar() {
   const closeToast = () => {
     setToast(false);
   };
+  useEffect(() => {
+    if (isToast) {
+      setToast(true);
+    }
+  }, [isToast]);
   return (
     <div className="relative">
       <form className="mt-14 flex flex-col mt-10 items-center w-full mb-4 md:flex-row md:px-16">
@@ -54,6 +52,14 @@ export default function SearchBar() {
           id="combo-box-demo"
           options={allResorts}
           value={resortName}
+          clearIcon={
+            <ClearIcon
+              onClick={() => {
+                handleSearch && setResortName("");
+              }}
+              fontSize="small"
+            />
+          }
           onChange={handleSelect}
           sx={{
             background: "white",
@@ -63,10 +69,14 @@ export default function SearchBar() {
           }}
           renderInput={(params) =>
             !toast ? (
-              <TextField {...params} label="Search for a resort..." />
+              <TextField
+                {...params}
+                onChange={(e) => console.log(params)}
+                label="Search for a resort..."
+              />
             ) : (
               <TextField
-              onClick={closeToast}
+                onClick={closeToast}
                 error
                 {...params}
                 id="outlined-error-helper-text"
@@ -75,8 +85,11 @@ export default function SearchBar() {
             )
           }
         />
+
         <button
-          onClick={(e) => showResort(e)}
+          onClick={(e) => {
+            handleSearch ? handleSearch(e, resortName) : showResort(e);
+          }}
           type="button"
           className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto  bg-blue-500 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
         >
