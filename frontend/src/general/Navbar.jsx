a51a1af70216bba9c6c7a30f986635664b3a5610
roeiu100 <roeiu100@gmail.com>
2023-03-08 16:52:53 +0200

    logdesign

--- frontend/src/general/Navbar.jsx
+++ frontend/src/general/Navbar.jsx
@@ -4,10 +4,21 @@ import SnowboardingIcon from "@mui/icons-material/Snowboarding";
 import { NavLink,useNavigate} from "react-router-dom";
 import LoginPage from "../userAuth/LoginPage";
 function Navbar() {
+  
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const [token, setToken] = useState(false);
- 
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    document.body.style.overflow = "auto";
+  };
   return (
     <>
       <nav className="bg-gray-800 ">
@@ -48,7 +59,22 @@ function Navbar() {
                     Country 1
                   </NavLink>
 
-               
+                  {!token ? (
+                    <div>
+                      <NavLink                        
+                        className="absolute top-4 right-2 text-gray-300 hover:bg-gray-700 focus:bg-gray-700 hover:text-white focus:outline-none focus:ring focus:ring-white-300 px-3 py-2 rounded-md text-sm font-medium"
+                        onClick={handleOpenModal}
+                      >
+                        LogIn
+                      </NavLink>
+                      <LoginPage
+                        open={modalOpen}
+                        onClose={handleCloseModal}
+                      ></LoginPage>
+                    </div>
+                  ) : (
+                    <div className="absolute  right-2"></div>
+                  )}
                 </div>
               </div>
             </div>
