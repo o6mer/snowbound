092c64d7559a0cf68602b180310f3cb74417bfa0
o6mer <o6merliraz@gmail.com>
2023-03-13 13:22:58 +0200

    merge fix

--- frontend/src/resort/components/ResortReviews.jsx
+++ frontend/src/resort/components/ResortReviews.jsx
@@ -13,55 +13,15 @@ const ResortReviews = ({ resortData }) => {
     <section className="">
       <p className="text-center flex items-center justify-center gap-2 text-lg font-bold p-2">
         {reviews?.length} Reviews{" "}
-        <Rating name="avg-rating" value={avgRatings || 4} readOnly />
+        {reviews?.length ? (
+          <Rating name="avg-rating" value={avgRatings} readOnly />
+        ) : null}
       </p>
       <div className="flex flex-col gap-2 overflow-y-scroll max-h-[50vh] ">
-        {reviews ? (
+        {reviews?.length ? (
           reviews?.map((review) => <Review {...review} />)
         ) : (
-          <>
-            <Review
-              title="review title"
-              body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde aperiam odit deleniti repellendus architecto! Excepturi quas at minima dolorem nesciunt? "
-              star={4}
-              vote={20}
-              resort_id="tynges"
-              date={new Date()}
-              poster={"omer"}
-              images={[
-                "https://static.savoie-mont-blanc.com/wp-content/uploads/external/d02d9289b11f5902eef5891085137110-14033081-1407x940.jpg",
-                "https://static.savoie-mont-blanc.com/wp-content/uploads/external/d02d9289b11f5902eef5891085137110-14033081-1407x940.jpg",
-              ]}
-            />
-            <Divider />
-            <Review
-              title="review title"
-              body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde aperiam odit deleniti repellendus architecto! Excepturi quas at minima dolorem nesciunt? "
-              star={4}
-              vote={20}
-              resort_id="tynges"
-              date={new Date()}
-              poster={"omer"}
-              images={[
-                "https://static.savoie-mont-blanc.com/wp-content/uploads/external/d02d9289b11f5902eef5891085137110-14033081-1407x940.jpg",
-              ]}
-            />
-            <Divider />
-
-            <Review
-              title="review title"
-              body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde aperiam odit deleniti repellendus architecto! Excepturi quas at minima dolorem nesciunt? "
-              star={4}
-              vote={20}
-              resort_id="tynges"
-              date={new Date()}
-              poster={"omer"}
-              images={[
-                "https://static.savoie-mont-blanc.com/wp-content/uploads/external/d02d9289b11f5902eef5891085137110-14033081-1407x940.jpg",
-              ]}
-            />
-            <Divider />
-          </>
+          <div className="">No Reviews</div>
         )}
       </div>
     </section>
