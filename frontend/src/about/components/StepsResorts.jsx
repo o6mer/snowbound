e1fd30e856e2c84dc5d8026c9416beb725625301
BPAdiv <adiv.balas@gmail.com>
2023-03-15 18:35:42 +0200

    Update StepsResorts.jsx

--- frontend/src/about/components/StepsResorts.jsx
+++ frontend/src/about/components/StepsResorts.jsx
@@ -125,7 +125,7 @@ const StepsResorts = () => {
     <>
       <section
         ref={ref}
-        className="header bg-white dark:bg-gray-900 lg:py-[5vw]"
+        className="header bg-gray-900 lg:py-[5vw]"
         id="HowToGetStarted"
       >
         <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 flex flex-col-reverse ">
