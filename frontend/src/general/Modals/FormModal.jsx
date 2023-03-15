07e64eb35c1b4377e99a8820b90b3d7595db824b
o6mer <o6merliraz@gmail.com>
2023-03-15 18:52:50 +0200

    fix

--- frontend/src/general/Modals/FormModal.jsx
+++ frontend/src/general/Modals/FormModal.jsx
@@ -1,99 +1,126 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import {useState} from "react"
-import axios from"axios"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+import axios from "axios";
 
+const FormModal = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
- const FormModal=()=> {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false)
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-     const [email,setEmail]=useState("")
-     const [subject,setSubject]=useState("")
-     const [message,setMessage]=useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, "22", subject, "33", message);
+    axios
+      .post("http://localhost:8000/api/user/thankyou", { emailme: email })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setOpen(false);
+  };
 
-     const handleSubmit =(e)=> {
-         e.preventDefault()
-         console.log(email, "22", subject, "33", message)
-            axios.post("http://localhost:8000/api/user/thankyou",{emailme:email})
-                .then(res=> {
-                    console.log(res.data)
-                })
-                .catch(err=> {
-                    console.log(err)
-                })
-         setOpen(false)
-
-    }
-
-
-    return (
-        <>
-            <button
-
-                className="text-blue-700  hover:text-blue-900"
-                onClick={()=>setOpen(true)}
-            >
-                Need help?
-            </button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-
-            >
-                {/*<div  className={" w-full h-full flex justify-center items-center " }>*/}
-                <section className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2  bg-white dark:bg-gray-900 w-max rounded px-[5vw]">
-                    <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact
-                            Us</h2>
-                        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Do you want some advice? <br/>  Want to send feedback about our website?  Need details about some
-                            of our resorts ? Let us know.</p>
-                        <form action="#" className="space-y-8" onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="email"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
-                                    email</label>
-                                <input type="email" id="email" onChange={(e)=>setEmail(e.target.value)}
-                                       className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                       placeholder="name@flowbite.com" required/>
-                            </div>
-                            <div>
-                                <label htmlFor="subject"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                                <input type="text" id="subject" onChange={(e)=>setSubject(e.target.value)}
-                                       className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                       placeholder="Let us know how we can help you" required/>
-                            </div>
-                            <div className="sm:col-span-2">
-                                <label htmlFor="message"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
-                                    message</label>
-                                <textarea id="message" rows="6" onChange={(e)=>setMessage(e.target.value)}
-                                          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                          placeholder="Leave a comment..."></textarea>
-                            </div>
-                            <div className="flex justify-between">
-                            <button type="submit"
-                                    className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-800 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
-                                message
-                            </button>
-                                <button onClick={()=>setOpen(false)}
-                                    className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-800 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Close
-
-                            </button>
-                            </div>
-                        </form>
-                    </div>
-                </section>
-                {/*</div>*/}
-            </Modal>
-        </>
-    );
-}
-export default FormModal
+  return (
+    <>
+      <button
+        className="text-blue-700  hover:text-blue-900 font-bold"
+        onClick={() => setOpen(true)}
+      >
+        Contact us
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        {/*<div  className={" w-full h-full flex justify-center items-center " }>*/}
+        <section className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2  bg-white dark:bg-gray-900 w-max rounded px-[5vw]">
+          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+              Contact Us
+            </h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+              Do you want some advice? <br /> Want to send feedback about our
+              website? Need details about some of our resorts ? Let us know.
+            </p>
+            <form action="#" className="space-y-8" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="name@flowbite.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="subject"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  onChange={(e) => setSubject(e.target.value)}
+                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                  placeholder="Let us know how we can help you"
+                  required
+                />
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                >
+                  Your message
+                </label>
+                <textarea
+                  id="message"
+                  rows="6"
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Leave a comment..."
+                ></textarea>
+              </div>
+              <div className="flex justify-between">
+                <button
+                  type="submit"
+                  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-800 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Send message
+                </button>
+                <button
+                  onClick={() => setOpen(false)}
+                  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-800 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Close
+                </button>
+              </div>
+            </form>
+          </div>
+        </section>
+        {/*</div>*/}
+      </Modal>
+    </>
+  );
+};
+export default FormModal;
