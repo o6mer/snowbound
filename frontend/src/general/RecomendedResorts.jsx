f4d8e562ff6a4f8513b84a16c5a64cad4a237182
o6mer <o6merliraz@gmail.com>
2023-03-15 18:11:32 +0200

    color changes

--- frontend/src/general/RecomendedResorts.jsx
+++ frontend/src/general/RecomendedResorts.jsx
@@ -1,13 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
-import { UserContext } from "../contexts/UserContextProvider";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import axios from "axios";
+import React from "react";
 import FavoriteButton from "./FavoriteButton.jsx";
 
 const RecomendedResorts = ({ resortData }) => {
   return (
-
     <div id="recomendedImg" className={`w-full m-10 antialiased text-gray-900`}>
       <div>
         <img
@@ -18,7 +13,7 @@ const RecomendedResorts = ({ resortData }) => {
 
         <div className="">
           <div className="relative px-4 -mt-16">
-            <div className="flex items-center flex-col w-full bg-white p-6 rounded-lg shadow-lg text-center transition ease-in-out delay-150  hover:-translate-y-2 hover:scale-100 duration-300">
+            <div className="flex items-center flex-col w-full bg-white p-6 rounded-lg shadow-lg text-center transition ease-in-out delay-150  hover:scale-100 duration-300">
               <div className="flex items-baseline w-full">
                 <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
                   New
@@ -41,7 +36,7 @@ const RecomendedResorts = ({ resortData }) => {
                 {resortData?.description}$
               </div>
               <a
-                className="mt-4 rounded-lg  p-2 bg-slate-100 w-max "
+                className="mt-4 rounded-lg  p-2 bg-teal-200 hover:bg-teal-400 transition-all w-max "
                 href={`/resort/${resortData?.name}`}
               >
                 Learn More
